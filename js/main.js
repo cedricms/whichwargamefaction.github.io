6e869236fdc46d04c3235c9e4ac7e16ed59fb6ce
Anonymous <whichwargamefaction@gmail.com>
2016-03-16 15:06:41 +0000

    Fixes the armies.json path.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var app = angular.module("wargameApp", []);
 
 app.controller("ArmyController", function($scope, $http) {
-  $http.get('./json/armies.json')
+  $http.get('./js/json/armies.json')
        .then(function(response){
     $scope.armies = response.data;
   });
